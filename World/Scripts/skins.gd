1c51108dc53993e2713df4ab7593e89e2db4236a
StrawKatt11 <ezegyemail1000@gmail.com>
2026-01-27 13:36:24 +0100

    billentyuzet

--- World/Scripts/skins.gd
+++ World/Scripts/skins.gd
@@ -2,12 +2,12 @@ extends Node
 
 
 var selected_skin_index = 0
-@export var skins = [
-	Color(1.0, 1.0, 1.0),
+var skins = [
+	Color(0.3,0.3,0.3),
 	Color(1.0, 0.0, 0.0),
 	Color(0.0, 1.0, 0.0),
 	Color(0.0, 0.0, 1.0),
 	Color(0.784, 0.588, 0.0),
 	Color(1.0, 1.0, 1.0, 0.2),
-	Color(0.0,0.0,0.0)
+	Color(1.0, 1.0, 1.0)
 ]
